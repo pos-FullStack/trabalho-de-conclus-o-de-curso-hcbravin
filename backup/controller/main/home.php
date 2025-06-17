2d8b7a9659d21b6d0f8399faa7a554755e9314a8
Henrique <98990825+hcbravin@users.noreply.github.com>
2025-06-17 13:32:27 -0300

    Delete backup directory

--- backup/controller/main/home.php
+++
@@ -1,1 +0,0 @@
-<?php require_once Views.'/main/home.php';
--- backup/views/html/aviso_NovaAgenda.php
+++
@@ -1,3 +0,0 @@
-<div class="infomain mb-1 bd-1 bd-info border-info">
-	As agendas mudaram. Mas calma! Você não terá dificuldade nenhuma de usar ela. Mas fique atento, você precisará informar a hora que o evento da sua agenda irá ocorrer.
-</div>
